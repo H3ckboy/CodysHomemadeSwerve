8e1adb3e7099604f95843413824a4495242b2a45
H3ckboy <boggsc716@gmail.com>
2024-02-28 17:34:16 -0500

    Codys Swerve Test v1.1

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -13,20 +15,20 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class Robot extends TimedRobot {
+  private RobotContainer robotContainer;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-
+    robotContainer = new RobotContainer();
   }
 
   @Override
   public void robotPeriodic() {
-
+    CommandScheduler.getInstance().run();
   }
-
   @Override
   public void autonomousInit() {
 
